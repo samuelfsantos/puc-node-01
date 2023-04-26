7fd73d080957610a27805000e8d673dd6dd5d9a8
Samuel Ferreira <1045936@sga.pucminas.br>
2023-04-26 14:12:53 -0300

    inclusão de novos produtos

--- src/models/produto.model.js
+++ src/models/produto.model.js
@@ -5,6 +5,8 @@ const produtos = [
     { id: 3, descricao: "Iogurte Natural 200ml", valor: 2.50, marca: "Itambé"  },
     { id: 4, descricao: "Batata Maior Palha 300gr", valor: 15.20, marca: "Chipps"  },
     { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé"  },
+    { id: 6, descricao: "Cerveja Long Neck 330ml", valor: 6.49, marca: "Heineken"  },
+    { id: 7, descricao: "Picanha 800gr", valor: 59.90, marca: "Friboi"  }
 ];
 
 module.exports = produtos;
