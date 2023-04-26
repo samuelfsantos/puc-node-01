8bd6f4a2df18412b13cbe5a47831ebfb1cd2c51c
Samuel Ferreira <1045936@sga.pucminas.br>
2023-04-26 02:19:22 -0300

    ajustes gerais projeto

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:18-alpine
+FROM node:14
 
 RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
 WORKDIR /home/node/app
@@ -7,7 +7,7 @@ COPY package*.json ./
 
 COPY --chown=node:node . .
 
-#EXPOSE 3000
+EXPOSE 3000
 #EXPOSE 80
 #EXPOSE 443
 
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const server = express();
 
-// parse requests of content-type - application/x-www-form-urlencoded
 server.use(bodyParser.urlencoded({ extended: true }));
-// parse requests of content-type - application/json
 server.use(bodyParser.json());
+server.use(cors());
 // versionamento
 const v1 = require('./src/routes/produto.route.js');
 server.use('/api/v1/', v1);
